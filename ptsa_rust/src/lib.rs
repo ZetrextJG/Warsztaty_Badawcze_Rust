bae4a80c7c23f7334d6171f963bb537e974d6c2c
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-08-13 19:52:39 +0200

    Add a basic module library

--- ptsa_rust/src/lib.rs
+++ ptsa_rust/src/lib.rs
@@ -1,99 +1,76 @@
-use pyo3::{prelude::*, pyclass_init::PyNativeTypeInitializer};
-use std::sync::{Arc, Mutex};
-use std::thread;
-use rand_distr::{Distribution, Beta};
+use chrono::Utc;
+use const_env::from_env;
+use pyo3::prelude::*;
+use utils::{
+    matrix::DistanceMatrix,
+    path::Solution,
+    state::{State, StatesContainer},
+    temp::TemperatureBounds,
+};
+mod utils;
 
-
-// calculate_distance.py
-fn cycle_length(cycle: &Vec<usize>, distnace_matrix: &Vec<Vec<f64>>) -> f64 {
-    // Calculate the length of that cycle using the distance matrix
-    //
-    let mut length = 0.0;
-    for i in 0..cycle.len() {
-        let j = (i + 1) % cycle.len();
-        length += distnace_matrix[cycle[i]][cycle[j]];
-    }
-    length += distnace_matrix[cycle[cycle.len() - 1]][cycle[0]];
-    return length;
-}
-
-// cooling.py
-fn cooling(temperature: f64, cooling_rate: &f64, min_temperature: &f64) -> f64 { 
-    let new_temperature = temperature * cooling_rate;
-    return f64::max(new_temperature, min_temperature.clone());
-}
-
-// initialization.py
-fn initialize_temperatures(n:&usize, min: &f64, max: &f64, a: &f64, b: &f64) -> Vec<f64> {
-    // Probably suboptimal, but it works
-    let beta = Beta::new(a.clone(), b.clone()).unwrap();
-    let beta_samples: Vec<f64> = beta.sample_iter(rand::thread_rng()).take(n.clone()).collect();
-    beta_samples
-        .iter()
-        .map(|x| x * (max - min) + min)
-        .collect()
-}
-
-fn initialize_transition_function_types(n: &usize, probability_of_shuffle: &f64) -> Vec<bool> {
-    (0..n.clone())
-        .map(|_| rand::random::<f64>() < probability_of_shuffle.clone())
-        .collect()
-}
-
-
-// metropolis_transition.py
-fn acceptance(solution_length: &f64, new_solution_length: &f64, temperature: &f64) -> bool {
-    let acceptance_probability = f64::exp(-(new_solution_length - solution_length) / temperature);
-    rand::random::<f64>() < f64::min(1.0, acceptance_probability)
-}
-
+#[from_env]
+const DIMENSION: usize = 10;
 
 #[pyclass]
 #[derive(Clone)]
 pub struct Parameters {
-    min_temperature: f64,
-    max_temperature: f64,
-    probability_of_shuffle: f64,
-    probability_of_heuristic: f64,
-    max_runtime_sec: f64,
-    number_of_concurrent_threads: i32,
-    max_length_percent_of_cycle: f64,
-    swap_states_probability: f64,
-    closeness: f64,
-    cooling_rate: f64
+    pub min_temperature: f64,
+    pub max_temperature: f64,
+    pub probability_of_shuffle: f64,
+    pub probability_of_heuristic: f64,
+    pub number_of_concurrent_threads: i32,
+    pub max_length_percent_of_cycle: f64,
+    pub swap_states_probability: f64,
+    pub closeness: f64,
+    pub cooling_rate: f64,
 }
 
-type Matrix = Vec<Vec<f64>>;
 #[pyclass]
 pub struct PtsaAlgorithm {
-    distnace_matrix: Arc<Mutex<Matrix>>,
-    parameters: Arc<Mutex<Parameters>>,
-
-    // initial_temperatures: Arc<Mutex<Vec<f64>>>,
-
-    best_solution: Vec<usize>,
-    best_solution_length: f64
+    pub parameters: Parameters,
 }
 
 impl PtsaAlgorithm {
-    pub fn run(&self) {
+    fn run<const N: usize>(
+        &self,
+        distance_matrix: DistanceMatrix<N>,
+        deadline: i64,
+    ) -> Solution<N> {
+        let best_solution = Solution::random_solution();
+        let temp_bounds = TemperatureBounds {
+            max: self.parameters.max_temperature,
+            min: self.parameters.min_temperature,
+        };
+        let states: StatesContainer<N> = StatesContainer::new(temp_bounds, distance_matrix);
+        // let state: State<N> = State {};
 
+        loop {
+            // This comparation might be a bottleneck
+            // Break condition
+            if Utc::now().timestamp() >= deadline {
+                return best_solution;
+            }
+        }
     }
 }
 
 #[pymethods]
 impl PtsaAlgorithm {
     #[new]
-    pub fn new(distnace_matrix: Vec<Vec<f64>>, parameters: Parameters) -> Self {
-        PtsaAlgorithm {
-            distnace_matrix: Arc::new(Mutex::new(distnace_matrix)),
-            // initial_temperatures: Arc::new(Mutex::new(initial_temperatures)),
-            parameters: Arc::new(Mutex::new(parameters)),
-            best_solution: vec![],
-            best_solution_length: f64::MAX
-        }
+    pub fn new(parameters: Parameters) -> Self {
+        PtsaAlgorithm { parameters }
     }
 
+    pub fn run_till(&self, matrix: Vec<Vec<f64>>, deadline_timestamp: String) -> Vec<usize> {
+        // Run the PTSA algorith on a given distance matrix till the specified deadline
+        // Deadtime must be a string containing the number of seconds from the start of unix time.
+        // Returns the best solution
+        let timestamp = deadline_timestamp.parse::<i64>().unwrap();
+        let dmatrix: DistanceMatrix<DIMENSION> = matrix.into();
+        let best_solution = self.run(dmatrix, timestamp);
+        best_solution.path.into()
+    }
 }
 
 /// A Python module implemented in Rust.
