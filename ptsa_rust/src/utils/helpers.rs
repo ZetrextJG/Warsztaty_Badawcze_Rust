74631d5d1b195f8bea41da26a52333d43b161df3
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-08-13 19:51:05 +0200

    Implement unique counter for iterator

--- ptsa_rust/src/utils/helpers.rs
+++ ptsa_rust/src/utils/helpers.rs
@@ -1,4 +1,4 @@
-fn initialize_transition_function_types(n: &usize, probability_of_shuffle: &f64) -> Vec<bool> {
+pub fn initialize_transition_function_types(n: &usize, probability_of_shuffle: &f64) -> Vec<bool> {
     (0..*n)
         .map(|_| rand::random::<f64>() < *probability_of_shuffle)
         .collect()
@@ -8,4 +8,22 @@ fn initialize_transition_function_types(n: &usize, probability_of_shuffle: &f64)
 pub fn acceptance(solution_length: f64, new_solution_length: f64, temperature: f64) -> bool {
     let acceptance_probability = f64::exp(-(new_solution_length - solution_length) / temperature);
     rand::random::<f64>() < f64::min(1.0, acceptance_probability)
+}
+
+pub trait CountUnique {
+    fn unique(self) -> usize;
+}
+
+impl<I, T> CountUnique for I
+where
+    I: Iterator<Item = T>,
+    T: Ord,
+{
+    fn unique(self) -> usize {
+        // Returns the number of unique elements in a iterator
+        let mut copy: Vec<T> = self.collect();
+        copy.sort();
+        copy.dedup();
+        copy.len()
+    }
 }
