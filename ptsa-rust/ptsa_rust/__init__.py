fbd88fc300da056dd11dc1184e9896d0d9f6da60
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-09-10 23:52:43 +0200

    Small code cleanup

--- ptsa-rust/ptsa_rust/__init__.py
+++ ptsa-rust/ptsa_rust/__init__.py
@@ -5,12 +5,11 @@ from .ptsa_rust import *
 
 
 def run_ptsa(distance_matrix: list[list[float]], time_s: int = 60, **kwargs) -> Result:
-    "possibilities"
     """
     Run the PTSA algorithm on a given distance matrix
     for specified about of time (in seconds).
 
-    Any additional keyword arguments will be passed to the 
+    Any additional keyword arguments will be passed to the
     params object `ptsa_rust.parameters.Parameters` and used by the runner.
     """
     params = Parameters(**kwargs)
--- ptsa-rust/src/lib.rs
+++ ptsa-rust/src/lib.rs
@@ -17,72 +17,70 @@ pub struct PtsaAlgorithm {
 }
 
 impl PtsaAlgorithm {
-    fn run(&self, distance_matrix: DistanceMatrix, deadline: i64) -> (Solution, f64) {
-        let size = distance_matrix.size;
-
-        let temp_bounds = TemperatureBounds {
-            max: self.params.max_temperature,
-            min: self.params.min_temperature,
-        };
-        let mut all_heuristic_solutions: Vec<(Solution, f64)> = (0..size)
-            .map(|starting_city| {
-                Solution::nearest_neightbor_solution(&distance_matrix, starting_city)
-            })
+    #[inline]
+    fn get_best_heuristic_solutions(&self, dmatrix: &DistanceMatrix) -> Vec<Solution> {
+        let mut all_heuristic_solutions: Vec<(Solution, f64)> = (0..dmatrix.size)
+            .map(|starting_city| Solution::nearest_neightbor_solution(dmatrix, starting_city))
             .map(|solution| {
-                let cost = solution.cost(&distance_matrix);
+                let cost = solution.cost(dmatrix);
                 (solution, cost)
             })
             .collect();
-
         all_heuristic_solutions.sort_by(|(_, a), (_, b)| a.total_cmp(b));
-
         let takes = (all_heuristic_solutions.len() as f64 * 0.1) as usize;
-        let heuristic_solutions: Vec<Solution> = all_heuristic_solutions
+        all_heuristic_solutions
             .into_iter()
             .take(takes)
             .map(|(solution, _)| solution)
-            .collect();
+            .collect()
+    }
 
+    fn run(&self, dmatrix: DistanceMatrix, deadline: i64) -> (Solution, f64) {
+        // Initialization
+        let size = dmatrix.size;
         let shuffle_bool_vector = initialize_transition_function_types(
             self.params.number_of_states,
             self.params.probability_of_shuffle,
         );
+        let temp_bounds = TemperatureBounds {
+            max: self.params.max_temperature,
+            min: self.params.min_temperature,
+        };
         let temperatures = temp_bounds.init_temperatures(
             self.params.number_of_states,
             self.params.temp_beta_a,
             self.params.temp_beta_b,
         );
+        let heuristic_solutions = self.get_best_heuristic_solutions(&dmatrix);
 
-        let mut states: StatesContainer = StatesContainer::new(temp_bounds, distance_matrix);
-
+        // Creating states
+        let mut states: StatesContainer = StatesContainer::new(temp_bounds, dmatrix);
         let mut rng = thread_rng();
         for (temperature, is_transion_shuffle) in temperatures.into_iter().zip(shuffle_bool_vector)
         {
-            if rng.gen_range(0.0..1.0) < self.params.probability_of_heuristic {
-                states.add(State {
-                    solution: heuristic_solutions.choose(&mut rng).unwrap().clone(),
-                    temperature,
-                    is_transion_shuffle,
-                })
+            let take_heuristic = rng.gen_range(0.0..1.0) < self.params.probability_of_heuristic;
+            let solution: Solution = if take_heuristic {
+                heuristic_solutions.choose(&mut rng).unwrap().clone()
             } else {
-                states.add(State {
-                    solution: Solution::random_solution(size),
-                    temperature,
-                    is_transion_shuffle,
-                })
-            }
+                Solution::random_solution(size)
+            };
+            let state = State {
+                solution,
+                temperature,
+                is_transion_shuffle,
+            };
+            states.add(state);
         }
 
-        let mut all_tries: usize = 0;
+        // Main loop
+        let mut loop_counter: usize = 0;
         loop {
-            // This comparison might be a bottleneck
             // Break condition
             if Utc::now().timestamp() >= deadline {
-                println!("This loop was run {} times", all_tries);
+                println!("This loop was run {} times", loop_counter);
                 return (states.best_solution.unwrap(), states.best_cost);
             }
-
-            all_tries += 1;
+            // Metropolis and replica transitions
             for _ in 0..self.params.number_of_repeats {
                 states.metropolis_tranision(self.params.max_length_percent_of_cycle);
                 for _ in 0..states.states.len() {
@@ -92,7 +90,10 @@ impl PtsaAlgorithm {
                     )
                 }
             }
+            // Cooling
             states.cool(self.params.cooling_rate);
+            // Additional
+            loop_counter += 1;
         }
     }
 }
